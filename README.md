7541f16c728cab3dd58098e087fdf000f806ef6d
Edward Thiel <edward.thiel@gmail.com>
2022-12-30 16:10:54 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # christmasTreeLights
 
 This project is developing the hardware and software to replace the light controller for a GE Christmas Tree.  The old controller broke and its the only part that you cant buy a replacement for.
+
+# Hardware Details
+
+## Parts List
+
+TODO
+
+## Wiring diagram
+
+TODO
+
+## End project
+
---
+++ christmasTree.ino
@@ -0,0 +1,105 @@
+int IN1=9;
+int IN2=8;
+int ENA=10;
+int MIN=10;
+int color = 0;
+int state = 1;
+
+void ChangeWhite() {
+    // This is white mode
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+}
+
+void ChangeColor() {
+    // This is color mode
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+}
+
+void ChangeState() {
+  if(color==1) {
+    ChangeWhite();
+    color = 0;
+  }
+  else {
+    ChangeColor();
+    color = 1;
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);  
+  pinMode(ENA,OUTPUT);
+  
+  if(color) {
+    ChangeColor();
+  }
+  else {
+    ChangeWhite();
+  }
+  int i = 0;
+  for(i = 0 ; i <= MIN; i+=1) {
+    analogWrite(ENA, i);
+    delay(5);
+  }
+
+
+}
+
+void Brighten() {
+  int i = 0;
+  for(i = MIN ; i <= 255; i+=1) { 
+    analogWrite(ENA, i);
+    delay(20);
+  }
+}
+
+void Dim() {
+  int i = 0;
+  for(i = 255; i >=MIN; i-=1) {
+    analogWrite(ENA, i);
+    delay(20);
+  }
+  
+}
+
+
+void Blinking() {
+  Brighten();
+  delay(10000);
+  Dim();
+  delay(5000);
+}
+
+void SwitchBlinking() {
+  Brighten();
+  delay(10000);
+  Dim();
+  delay(5000);
+  ChangeState();
+  Brighten();
+  delay(10000);
+  Dim();
+  delay(5000);
+  ChangeState();
+}
+
+
+void Solid() {
+  analogWrite(ENA, 255);
+}
+
+void loop() {
+  if(state == 0) {
+    Blinking();
+  }
+  else if(state==1) {
+    SwitchBlinking();
+  }
+  else {
+    Solid();
+  }
+}
